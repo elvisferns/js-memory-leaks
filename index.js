578739375871e0a0d5e13eb4ee43aa6ae30d889b
elvis <elvisfernandes.fernandes@kaplan.com>
2022-08-10 16:09:54 +0530

    add memory leaks examples

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+'use strict';
+
 class Car {
   constructor(name) {
     this.name = name;
   }
+
+  drive() {
+    console.log(`🚀 ~ file: index.js ~ line 9 ~ Car ~ drive ~ drive`, drive);
+  }
 }
 
-window.cars = [];
-const car1 = new Car('FORD');
-window.cars.push(car1);
+document.querySelector('#makeCar').addEventListener('click', () => {
+  if (!window.carObj1) {
+    window.cars = [];
+    window.carObj1 = new Car('FORD');
+    window.cars.push(window.carObj1);
+    window.alert('Car created successfully');
+  }
+});
+
+document.querySelector('#clearCar').addEventListener('click', () => {
+  window.carObj1 = null;
+});
+
+document.querySelector('#deleteCar').addEventListener('click', () => {
+  window.cars = window.cars.filter(car => car !== window.carObj1);
+  window.carObj1 = null;
+});
+
+
