0696425354406a21f4ad3809e3850f304754a1be
elvis <elvisfernandes.fernandes@kaplan.com>
2022-08-16 13:49:57 +0530

    memory leaks in ES6 collections

--- index.js
+++ index.js
@@ -29,3 +29,37 @@ document.querySelector('#deleteCar').addEventListener('click', () => {
 });
 
 
+let cars = [];
+let carsMap = new Map();
+let carsSet = new Set();
+document.querySelector('#makeCarCollection').addEventListener('click', () => {
+  if (!window.carObj1) {
+    cars = [];
+    carsMap.clear();
+    carsSet.clear();
+    window.carObj1 = new Car('FORD');
+    cars.push(window.carObj1);
+    carsMap.set('FORD', window.carObj1);
+    carsSet.add(window.carObj1);
+    window.alert('Car created in collection');
+  }
+});
+
+document.querySelector('#deleteCarCollection').addEventListener('click', () => {
+  cars = cars.filter(car => car !== window.carObj1);
+  carsMap.delete('FORD');
+  carsSet.delete(window.carObj1);
+  window.carObj1 = null;
+});
+
+let carsWeakMap = new WeakMap();
+let carsWeakSet = new WeakSet();
+document.querySelector('#makeCarWeakCollection').addEventListener('click', () => {
+  if (!window.carObj1) {
+    window.carObj1 = new Car('FORD');
+    carsWeakMap.set(window.carObj1, 'FORD');
+    carsWeakSet.add(window.carObj1);
+    window.alert('Car created in weak collections');
+  }
+});
+
